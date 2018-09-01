c0bf2a085911a3f8abe45e1760a72797067a7358
anurag.v70 <anurag.v70@wipro.com>
2018-09-01 23:30:33 +0530

    lastest

--- src/components/heroImage/index.js
+++ src/components/heroImage/index.js
@@ -44,19 +44,28 @@ class HeroImage extends React.Component{
         return(
             <div className="heroimage-main-div" >
                 <Header />
-                <p className="heroimage-maintext-p" >
-                    We Are
-                    <span id="heroimage-maintext-wordchangerid" className="heroimage-maintext-wordchanger" >
-                        {heroImageWords[heroImageWordIndex]}
-                    </span>
-                </p>
+                <div className="col-md-6" >
+                    <p className="heroimage-maintext-p" >
+                        We Craft Digital Solutions
+                        <span id="heroimage-maintext-wordchangerid" className="heroimage-maintext-wordchanger" >
+                            {heroImageWords[heroImageWordIndex]}
+                        </span>
+                    </p>
 
-                <p className="heroimage-subtext-p" > Empowering Digital Solutions for Tommorow's World. Empowering Digital Solutions for Tommorow's World.
-                Empowering Digital Solutions for Tommorow's World </p>
+                    <p className="heroimage-subtext-p" > Empowering Digital Solutions for Tommorow's World. Empowering Digital Solutions for Tommorow's World.
+                    Empowering Digital Solutions for Tommorow's World </p>
 
-                <p className="heroimage-downarrow-p" ><i className="fa fa-angle-double-down" aria-hidden="true"></i></p>
+                    <div className="heroimage-learnmore-div" >
+                        <button className="heroimage-learnmore-btn" >Learn more</button>
+                    </div>
+                </div>
+                <div className="col-md-6"  >
+                    <img className="heroimage-sideimage-img" width="550" height="300"   src={require("../../images/lappis1.png")} />
+                </div>
 
+                {/* <p className="heroimage-downarrow-p" ><i className="fa fa-angle-double-down" aria-hidden="true"></i></p> */}
 
+                
                 {/* change word for every 4 seconds */}
                 <div style={{display:"none"}} >
                    {
