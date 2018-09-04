c9d21e4e78798aa858f9b968bb05ff66b69a20a4
jayakumarreddy <rjayakumarreddy887@gmail.com>
2018-09-04 21:32:53 +0530

    footer changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 import HeaderMenu from "./components/HeaderMenu/newlinkHeaderMenu";
 import HeroImage from "./components/heroImage/index";
 import Ourservices from "./components/ourServices/index";
+import WhyChooseUs from "./components/whyChooseUs/index";
 import Footer from "./components/Footer/Footer";
 import AboutUs from "./components/Aboutus/index";
-import WhyChooseUs from  "./components/whyChooseUs/index";
 
 class App extends Component {
   render() {
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,40 +5,53 @@ import "./footer.css";
 class Footer extends Component {
     render() {
         return (
-            <footer className="footer">
-             <div className="footer-inner">
-               <div className="footer-content">
-                 <div className="col-md-6 col-xs-12 footer-item">
-                     <h3 className="footer-item-heading">newInk Digital Solutions</h3>
-                     <p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem accumsan varius montes viverra nibh in adipiscing. Lorem ipsum dolor vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis vol.</p>
-                 </div>
-                 <div className="col-md-3 footer-item">
-                    <h4 className="footer-item-heading">Importatant Links</h4>
-                    <ul className="footer-ul">
-                       <li><a href="#">Navigate</a></li><hr/>
-                       <li><a href="#">Navigate</a></li><hr/>
-                       <li><a href="#">Navigate</a></li><hr/>
-                       <li><a href="#">Navigate</a></li>
-                    </ul>
-                 </div>
-                 <div className="col-md-3 footer-item footer-social">
-				    <h4 className="footer-item-heading">Follow US</h4>
+            <div className="footer">
+                <div className="footer-content container">
+                 <div className="row">
+                   <div className="col-md-3 col-xs-12 col-sm-6">
+                        <h3 className="footer-item-heading">newInk Digital Solutions</h3>
+                        <p>We create possibilities for the connected world</p>
+                        <p>Build your Business with us</p>
+                        <button>Lets Talk</button>
+                    </div>
+                    <div className="col-md-3 col-xs-12 col-sm-6 footer-item footer-item-explore">
+                        <h4 className="footer-item-heading">Explore</h4>
+                        <ul className="footer-ul">
+                          <li><a href="#">About Us</a></li><hr/>
+                          <li><a href="#">Our Services</a></li><hr/>
+                          <li><a href="#">Technologies</a></li><hr/>
+                          <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-md-3 col-xs-12 col-sm-6 footer-item footer-social">
+				      <h4 className="footer-item-heading">Follow Us</h4>
 					  <ul class="footer-ul">
 						<li><a href="#"><i class="icon fa fa-twitter">&nbsp;&nbsp;</i>Twitter</a></li>
 						<li><a href="#"><i class="icon fa fa-facebook">&nbsp;&nbsp;</i>Facebook</a></li>
 						<li><a href="#"><i class="icon fa fa-instagram">&nbsp;&nbsp;</i>Instagram</a></li>
 						<li><a href="#"><i class="icon fa fa-github">&nbsp;&nbsp;</i>Github</a></li>
 					  </ul>
-			     </div>
-               </div>
-               <hr/>
-               <div className="footer-copyright">
-                    <div className="col-xs-12">
-                      <p>© NewInkTechnologies 2018 </p>
+			        </div>
+                    <div className="col-md-3 col-xs-12 col-sm-6 footer-item footer-social">
+				      <h4 className="footer-item-heading">Contact Us</h4>
+					  <ul class="footer-ul">
+						<li><i class="icon fa fa-location-arrow">&nbsp;&nbsp;</i>Hyderabad</li>
+						<li><i class="icon fa fa-phone">&nbsp;&nbsp;</i>+91-9948380255</li>
+						<li><i class="icon fa fa-envelope">&nbsp;&nbsp;</i>rjayakumarreddy887@gmail.com</li>
+					  </ul>
+			        </div>
+                 </div>
+                 <hr/>
+                 <div className="row">
+                    <div className="footer-copyright">
+                        <div className="col-xs-12">
+                        <p>COPYRIGHT © 2018 NewInk Technologies, INC.</p>
+                        <p>ALL RIGHTS RESERVED.</p>
+                        </div>
                     </div>
-               </div>
-             </div>
-            </footer>
+                 </div>
+                </div>
+            </div>
         );
     }
 }
